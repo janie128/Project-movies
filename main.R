1369a9477b6f066469c5502be8eafcf77b4de8c4
janie128 <janiec128@gmail.com>
2016-01-05 19:33:03 -0800

    Update main execution script

--- main.R
+++ main.R
@@ -5,3 +5,22 @@ boxOfficeRaw <- scrapingBOFn()
 # Call script to clean box office data
 source('./clean_explore_BOdata.R', echo=FALSE)
 boxOffice <- cleaningBOFn(boxOfficeRaw)
+
+# Call script to match product titles in metadata file to movie titles in box office data
+noTvMetadata <- 
+  
+# Call script to clean reviews + metadata files
+source('./clean_reviewsTitles.R', echo=FALSE)
+reviewsTitles <- cleanReviewsTitles(noTvMetadata)
+
+# Call script to extract features
+source('./feature_extraction.R', echo=FALSE)
+final <- extractFeatures(reviewsTitles)
+
+# Call script for modelling and prediction
+
+
+
+
+#g <- ggplot(filter(final, (reviewCount>50 & reviewCount<2000)), aes(x=goodAvgTFIDF, y=log10(adjGross), colour=adjGross))
+#g + geom_point() + scale_color_gradient(low="green", high="red")
